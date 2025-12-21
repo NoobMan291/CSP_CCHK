eaeb0c1fe9f43c383a1bc71ac1fdad701858046a
El Faria Omar <omar.el-faria@epita.fr>
2025-12-21 18:17:16 +0100

    feat: printing but characters not appearing on terminal

--- src/display/display.c
+++ src/display/display.c
@@ -11,4 +11,20 @@ void display(char *path) {
         exit(2);
     }
 
+    fseek(fptr, 0, SEEK_END);
+    long fsize = ftell(fptr);
+    char *string = malloc(fsize + 1);
+    fread(string, fsize, 1, fptr);
+
+    string[fsize] = 0;
+
+    
+
+    //while(1)
+    //{
+    printf("%s", string);
+    //}
+
+    fclose(fptr);
+    free(string);
 }
