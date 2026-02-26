f8787183100e7bfa6c04ac22bce76653070bab28
El Faria Omar <omar.el-faria@epita.fr>
2026-02-26 10:53:08 +0100

    feat: vectors finished. I think.

--- src/vector/vector.c
+++ src/vector/vector.c
@@ -15,14 +15,16 @@ struct Vector* add(struct Vector* u, struct Vector* v)
 
     float* values = calloc(size, sizeof(float));
 
-    for (int i = 0; i < u->size; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        values[i] = u->values[i];
-    }
-
-    for (int i = 0; i < v->size; i++)
-    {
-        values[i] = v->values[i];
+        if (u->size <= size)
+        {
+            values[i] = u->values[i];
+        }
+        if (v->size <= size)
+        {
+            values[i] += v->size;
+        }
     }
 
     struct Vector* res = malloc(sizeof(struct Vector));
@@ -30,5 +32,71 @@ struct Vector* add(struct Vector* u, struct Vector* v)
     res->size = size;
     res->values = values;
     
-    return values;
+    return res;
 }
+
+struct Vector* sub(struct Vector* u, struct Vector* v)
+{
+    for (size_t i = 0; i < v->size; i++)
+    {
+        v->values[i] = v->values[i] * 1;
+    }
+
+    return add(u, v);
+}
+
+struct Vector* scal(float k, struct Vector* u)
+{
+    for (size_t i = 0; i < u->size; i++)
+    {
+        u->values[i] = u->values[i] * k;
+    }
+
+    return u;
+}
+
+float leng(struct Vector* u)
+{
+    float res = 0;
+
+    for (size_t i = 0; u->size; i++)
+    {
+        res += u->values[i] * u->values[i];
+    } 
+
+    return sqrtf(res);
+}
+
+struct Vector* norm(struct Vector* u)
+{
+    float len = leng(u);
+
+    float* values = calloc(u->size, sizeof(float));
+
+    for (size_t i = 0; i < u->size; i++)
+    {
+        values[i] = u->values[i] / len;
+    }
+
+    struct Vector* res = malloc(sizeof(struct Vector));
+
+    res->size = u->size;
+    res->values = values;
+
+    return res;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/vector/vector.h
+++ src/vector/vector.h
@@ -1,17 +1,21 @@
 #pragma once
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+
 struct Vector {
     size_t size;
 
     float* values;
-}
+};
 
-struct Vector* add(struct Vector* u, struct Vector* v); // Addition of 2 vectors
+struct Vector* add(struct Vector* u, struct Vector* v); // Addition of 2 vectors. Will return a new pointer.
 
-struct Vector* sub(struct Vector* u, struct Vector* v); // Substraction of 2 vectors
+struct Vector* sub(struct Vector* u, struct Vector* v); // Substraction of 2 vectors. Will return a new pointer.
 
-struct Vector* scal(float k, struct Vector* u); // Scalar of a vector with a float k
+struct Vector* scal(float k, struct Vector* u); // Scalar of a vector with a float k. Will return the same pointer.
 
-float leng(struct Vector* u); // Length of a vector
+float leng(struct Vector* u); // Length of a vector. Will return a float, the given pointer will not be freed.
 
-struct Vector* norm(struct Vector* u); // Normalization of a vector
+struct Vector* norm(struct Vector* u); // Normalization of a vector. Will return a new pointer.
